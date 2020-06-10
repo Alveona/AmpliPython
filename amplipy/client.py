f884059375a7e8ef2af58e269c5e2ba5609f7820
Alveona <pomavau@yandex.ru>
2020-06-10 22:12:53 +0300

    Initial commit, Event class included

---
+++ amplipy/client.py
@@ -0,0 +1,12 @@
+
+class AmplipyClient():
+    api_key: str = None
+
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+
+    def log_event(self):
+        pass
+
+    def log_events(self):
+        pass
---
+++ amplipy/utils.py
@@ -0,0 +1,9 @@
+# list of Amplitude's reserved event types
+EVENT_TYPE_BLACKLIST = [
+    "[Amplitude] Start Session",
+    "[Amplitude] End Session",
+    "[Amplitude] Revenue",
+    "[Amplitude] Revenue (Verified)",
+    "[Amplitude] Revenue (Unverified)",
+    "[Amplitude] Merged User",
+]
